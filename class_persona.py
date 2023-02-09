27991530199d20a17488c4bc29ba849ebeabad47
alejandr0valle <alejandro.ovalle>
2023-02-08 22:50:51 -0500

    persona

---
+++ class_persona.py
@@ -0,0 +1,9 @@
+class persona:
+    def __init__(self, edad, genero, etnia):
+        self.persona = edad
+        self.genero = genero
+        self.etnia = etnia
+
+
+    def altura(sefl, centimetros_de_altura):
+        print("Usted mide: ", centimetros_de_altura)
